8d6f57d49b63dfa9009318a53aab1a0b7f4706ce
SANTIAGO VARELA JIM�NEZ <santiagovarela314184@correo.itm.edu.co>
2023-02-22 13:32:31 -0500

    Creating new concret class called SalaryEmployee

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    internal class SalaryEmployee : Employee
+    {
+        #region properties
+
+        public decimal Salary { get; set; }
+
+
+
+
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{} \n\t Value to pay: {}";
+            //39:24
+        }
+
+        #endregion
+
+
+    }
+}
